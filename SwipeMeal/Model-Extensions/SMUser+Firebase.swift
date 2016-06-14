4a11b6ab61bbffaa38a150eca385943e55e89889
gklei <gregoryhklein@gmail.com>
2016-06-14 14:20:38 -0400

    Added ...several files

---
+++ SwipeMeal/Model-Extensions/SMUser+Firebase.swift
@@ -0,0 +1,9 @@
+//
+//  SMUser+Firebase.swift
+//  SwipeMeal
+//
+//  Created by Gregory Klein on 6/9/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+import Foundation
---
+++ SwipeMeal/Operations/CreateUserAccountOperation.swift
@@ -0,0 +1,13 @@
+//
+//  CreateUserAccountOperation.swift
+//  SwipeMeal
+//
+//  Created by Gregory Klein on 6/9/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+import UIKit
+
+class CreateUserAccountOperation: BaseOperation {
+
+}
