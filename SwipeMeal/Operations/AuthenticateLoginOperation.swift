e076588389d6a7ea0d92bcbff217302ab3fe0e51
gklei <gregoryhklein@gmail.com>
2016-06-16 16:36:58 -0400

    Implemented simple Authenticate login operation class

--- SwipeMeal/Operations/AuthenticateLoginOperation.swift
+++ SwipeMeal/Operations/AuthenticateLoginOperation.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class AuthenticateLoginStatus
+{
+   let email: String
+   let password: String
+   
+   var user: SwipeMealUser?
+   var error: NSError?
+   
+   init(email: String, password: String)
+   {
+      self.email = email
+      self.password = password
+   }
+}
+
+class AuthenticateLoginOperation: BaseOperation
+{
+   private let _status: AuthenticateLoginStatus
+   
+   init(status: AuthenticateLoginStatus)
+   {
+      _status = status
+   }
+   
+   override func execute()
+   {
+      SMAuthLayer.signIn(_status.email, password: _status.password) { (user, error) in
+         self._status.user = user
+         self._status.error = error
+         self.finish()
+      }
+   }
+}
