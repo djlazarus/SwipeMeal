3c86f8c7f9d418c3642bdaa10d9c32a6d36180d8
gklei <gregoryhklein@gmail.com>
2016-06-20 18:07:44 -0400

    Showing action sheet after the plus button is pressed

--- SwipeMeal/ViewControllers/AddProfileImageViewController.swift
+++ SwipeMeal/ViewControllers/AddProfileImageViewController.swift
@@ -24,4 +24,28 @@ class AddProfileImageViewController: UIViewController
       _imageView.tintColor = UIColor(white: 0.9, alpha: 1)
       _imageView.image = image
    }
+   
+   // MARK: - Actions
+   @IBAction private func _addImageButtonPressed()
+   {
+      let alertController = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+      
+      let photoLibraryAction = UIAlertAction(title: "Photo Library", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction!) in
+         print("photo library")
+      })
+      
+      let takePictureAction = UIAlertAction(title: "Take Photo", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction!) in
+         print("take photo")
+      })
+      
+      let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: {(alert: UIAlertAction!) in
+         print("cancel")
+      })
+      
+      alertController.addAction(photoLibraryAction)
+      alertController.addAction(takePictureAction)
+      alertController.addAction(cancelAction)
+      
+      self.presentViewController(alertController, animated: true, completion:{})
+   }
 }
