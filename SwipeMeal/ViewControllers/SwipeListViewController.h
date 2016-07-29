5025d085567ff1e14a50589df639f0162b5071df
Jacob Harris <jacob@jacobharris.org>
2016-07-29 18:32:08 -0400

    Buying screen setup

---
+++ SwipeMeal/ViewControllers/SwipeListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SwipeListViewController.h
+//  SwipeMeal
+//
+//  Created by Jacob Harris on 7/29/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwipeListViewController : UIViewController
+
+@end
---
+++ SwipeMeal/ViewControllers/SwipeListViewController.m
@@ -0,0 +1,22 @@
+//
+//  SwipeListViewController.m
+//  SwipeMeal
+//
+//  Created by Jacob Harris on 7/29/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+#import "SwipeListViewController.h"
+
+@interface SwipeListViewController ()
+
+@end
+
+@implementation SwipeListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
