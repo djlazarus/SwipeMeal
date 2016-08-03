a64ea23d660c8fbf1a66f828650df7d34dbc9865
Jacob Harris <jacob@jacobharris.org>
2016-08-02 20:19:13 -0400

    Swipe buy detail WIP

--- SwipeMeal/ViewControllers/SwipeBuyDetailViewController.m
+++ SwipeMeal/ViewControllers/SwipeBuyDetailViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "SwipeBuyDetailViewController.h"
+#import "SwipeMeal-Swift.h"
 
 @interface SwipeBuyDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet CircularImageView *mainImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *swipesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *salesLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *ratingImageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *ratingImageView2;
+@property (weak, nonatomic) IBOutlet UIImageView *ratingImageView3;
+@property (weak, nonatomic) IBOutlet UIImageView *ratingImageView4;
+@property (weak, nonatomic) IBOutlet UIImageView *ratingImageView5;
 
 @end
 
@@ -16,11 +27,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)setSwipe:(Swipe *)swipe {
-    NSLog(@"got a swipe! %@", swipe);
+    
+    self.nameLabel.text = self.swipe.sellerName;
+    self.mainImageView.image = [UIImage imageNamed:@"temp-greg"];
+    self.swipesLabel.text = @"7";
+    self.salesLabel.text = @"42";
+    
+    NSArray *stars = @[self.ratingImageView1, self.ratingImageView2, self.ratingImageView3, self.ratingImageView4, self.ratingImageView5];
+    for (int i = 0; i < self.swipe.sellerRating; i++) {
+        UIImage *starImage = [UIImage imageNamed:@"buy-star"];
+        UIImageView *starImageView = [stars objectAtIndex:i];
+        starImageView.image = [starImage imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        starImageView.tintColor = [UIColor colorWithRed:184.0/255 green:184.0/255 blue:184.0/255 alpha:1.0];
+    }
 }
 
 @end
