857d8075c7a6dc8e23b342bbbfb9714c0c9886a8
gklei <gregoryhklein@gmail.com>
2016-08-15 14:01:26 -0400

    Added user profile image to the profile view

--- SwipeMeal/ViewControllers/ProfileViewController.swift
+++ SwipeMeal/ViewControllers/ProfileViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import FirebaseStorage
+import IncipiaKit
 
 class ProfileViewController: UIViewController {
+	
+	@IBOutlet private var _backgroundImageView: UIImageView!
+	@IBOutlet private var _centerImageView: CircularImageView!
 	
 	// MARK: - Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		_backgroundImageView.alpha = 0.5
+		guard let user = SMAuthLayer.currentUser else { return }
+		
+		let storage = FIRStorage.storage()
+		let path = "profileImages/\(user.uid).jpg"
+		let pathRef = storage.referenceWithPath(path)
+		pathRef.dataWithMaxSize(1024 * 1024) { (data, error) in
+			
+			guard let data = data else { return }
+			let image = UIImage(data: data)
+			self._centerImageView.image = image
+			self._backgroundImageView.image = image?.applyBlur(withRadius: 4, tintColor: nil, saturationDeltaFactor: 1.2)
+		}
+	}
+	
+	override func preferredStatusBarStyle() -> UIStatusBarStyle {
+		return .LightContent
 	}
 	
 	// MARK: - Actions
